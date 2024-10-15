e21853a4412ea832db9bdb70263133c2e2665d79
AnnaDoi <willow6510@yahoo.co.jp>
2024-10-15 16:26:11 +0900

    以前のデータを復元

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('tests/test', [ TestController::class, 'index' ]);
 Route::get('/', function () {
     return view('welcome');
 });
