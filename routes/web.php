b45dbbecb35d805c960f63ae2c66073a43493d02
AnnaDoi <willow6510@yahoo.co.jp>
2024-10-15 16:42:03 +0900

    ルートの書き方

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestController;
+use App\Http\Controllers\ContactFormController;
+use App\Models\ContactForm;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +18,16 @@ use App\Http\Controllers\TestController;
 */
 
 Route::get('tests/test', [ TestController::class, 'index' ]);
+
+// Route::resource('contacts', ContactFormController::class);
+
+Route::prefix('contacts')->middleware(['auth'])
+->controller(ContactFormController::class)
+->name('contacts')
+->group(function(){
+    Route::get('/', 'index')->name('index');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
