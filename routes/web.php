8ccce9c7985ee9eeddf4af45120a834bfa1616c9
AnnaDoi <willow6510@yahoo.co.jp>
2024-10-15 17:24:11 +0900

    ContactのCreate ActionとMVC追加

--- routes/web.php
+++ routes/web.php
@@ -21,11 +21,13 @@ Route::get('tests/test', [ TestController::class, 'index' ]);
 
 // Route::resource('contacts', ContactFormController::class);
 
-Route::prefix('contacts')->middleware(['auth'])
+Route::prefix('contacts')
+->middleware(['auth'])
 ->controller(ContactFormController::class)
-->name('contacts')
+->name('contacts.')
 ->group(function(){
     Route::get('/', 'index')->name('index');
+    Route::get('/create', 'create')->name('create');
 });
 
 Route::get('/', function () {
