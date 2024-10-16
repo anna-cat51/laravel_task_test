f40c45da6e3775658a5f77c172ef665720178550
AnnaDoi <willow6510@yahoo.co.jp>
2024-10-16 15:39:19 +0900

    storeアクション追加

--- resources/views/contacts/create.blade.php
+++ resources/views/contacts/create.blade.php
@@ -10,7 +10,7 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                 <section class="text-gray-600 body-font relative">
-                    <form method="post" action="">
+                    <form method="post" action="{{ route('contacts.store') }}">
                         @csrf
                         <div class="container px-5  mx-auto">
                             <div class="lg:w-1/2 md:w-2/3 mx-auto">
@@ -30,7 +30,7 @@
 
                                 <div class="p-2 w-full">
                                 <div class="relative">
-                                    <label for="email" class="leading-7 text-sm text-gray-600">メールemail</label>
+                                    <label for="email" class="leading-7 text-sm text-gray-600">メールアドレス</label>
                                     <input type="email" id="email" name="email" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                                 </div>
                                 </div>
