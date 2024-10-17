d591f2c03200b234ef86461e8e9e3eaba115c564
AnnaDoi <willow6510@yahoo.co.jp>
2024-10-17 15:08:34 +0900

    多対多のアソシエーション

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Area;
 use App\Models\Shop;
-
+use App\Models\Route;
 class ShopController extends Controller
 {
     public function index()
@@ -13,8 +13,11 @@ class ShopController extends Controller
         //1対多 親->子
         $shops = Area::find(1)->shops;
         // 親 <- 子
-
         $area = Shop::find(3)->area;
-        dd($shops,$area);
+
+        // 多対多
+        $routes = Shop::find(1)->routes()->get();
+
+        dd($shops,$area, $routes);
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,7 +22,9 @@ class DatabaseSeeder extends Seeder
             TestSeeder::class,
             UserSeeder::class,
             AreaSeeder::class,
-            ShopSeeder::class
+            ShopSeeder::class,
+            RouteSeeder::class,
+            RouteShopSeeder::class
         ]);
         \App\Models\ContactForm::factory(100)->create();
 
